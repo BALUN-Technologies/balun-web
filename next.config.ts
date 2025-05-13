343ffaa3533d055c34aecff6f898f1878db90282
armanzkhan <armanzaman21@gmail.com>
2025-05-13 18:23:03 +0500

    feat: added the website

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // Ensures proper build structure
+  trailingSlash: true, // Helps with static exports
+  eslint: {
+    ignoreDuringBuilds: true, // Skips linting warnings during production builds
+  },
 };
 
 export default nextConfig;
